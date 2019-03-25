d9b21fd3b97993c22c14344d6073f38535c55935
jaywood128 <jaywood128@gmail.com>
2019-03-25 10:14:40 -0700

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class PostsController < ApplicationController
   def index
     @posts = Post.all
@@ -5,7 +6,6 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
-    redirect_to new_post_path(@post)
   end
 
   def new
@@ -13,9 +13,11 @@ class PostsController < ApplicationController
   end
 
   def create 
+   
     @post = Post.new
     @post.title = params[:title]
     @post.description = params[:description]
     @post.save
+    redirect_to post_path(@post)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-resources :posts, only: [:index, :show]
-
+resources :posts, only: [:index, :show, :new]
+post '/posts', to: 'posts#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
